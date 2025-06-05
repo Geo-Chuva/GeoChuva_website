155379eaa0ec031d89ca86afe210ec4446d37f6a
Thaynafp <tflopes01@gmail.com>
2025-06-04 21:40:58 -0300

    teste mudar tema

--- js/script.js
+++ js/script.js
@@ -51,3 +51,23 @@ setInterval(proximoSlide, 4000);
 
 
 //
+
+
+function mudarTema(tema) {
+    if (tema === 'claro') {
+        document.documentElement.style.setProperty('--background-color', '#E2E6F9');
+        document.documentElement.style.setProperty('--color-topicos', '#1F69B4');
+        document.documentElement.style.setProperty('--color-text', '#FFFFFF');
+        document.body.style.background = '#E2E6F9';
+    } else if (tema === 'escuro') {
+        document.documentElement.style.setProperty('--background-color', '#222831');
+        document.documentElement.style.setProperty('--color-topicos', '#393E46');
+        document.documentElement.style.setProperty('--color-text', '#EEEEEE');
+        document.body.style.background = '#222831';
+    } else if (tema === 'azul') {
+        document.documentElement.style.setProperty('--background-color', '#cce6ff');
+        document.documentElement.style.setProperty('--color-topicos', '#00509e');
+        document.documentElement.style.setProperty('--color-text', '#FFFFFF');
+        document.body.style.background = '#cce6ff';
+    }
+}
